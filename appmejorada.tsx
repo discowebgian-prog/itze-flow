d7b2ce588373d66f647ab74be2dad4214257e6c2
discowebgian-prog <discowebgian@gmail.com>
2026-07-30 11:15:39 -0600

    Update appmejorada.tsx

--- appmejorada.tsx
+++ appmejorada.tsx
@@ -4371,7 +4371,7 @@ function Dashboard({
             bg: '#FEF2F2',
           },
           {
-            label: 'Ocupación de hoy',
+            label: 'Ocupación hoy',
             val: (() => {
               const totalRooms = properties.reduce((s, p) => s + (p.rooms || 1), 0);
               return totalRooms > 0 
